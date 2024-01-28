869faa568bc3dfd9d96a82c4faf43da4153f0ce5
Aktan <aktanberditop@gmail.com>
2024-01-28 23:32:40 +0600

    Second

---
+++ Lab2/lists.py
@@ -0,0 +1,18 @@
+#aktan
+
+#ex 1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[2])
+
+#ex 2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+#ex 3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#ex 4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
